7816f38d114c15ba935eb9db1a39482e52e5ce3e
BeOKPay <be.okpayo4@gamil.com>
2026-09-10 17:34:10 +0530

    Refactor URL configuration for improved readability
    - Simplified the addition of static media URL handling in urls.py by using a more concise format for appending to urlpatterns.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,7 @@ urlpatterns = [
     path("api/v1/admin/", include("apps.dashboard.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
